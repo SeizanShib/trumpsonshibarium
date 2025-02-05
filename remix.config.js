44ea8ebd1abb0d87e4e5cc029eefcee28a21ad68
SeizanShib <153096089+SeizanShib@users.noreply.github.com>
2025-02-05 22:07:33 +0100

    Deploying updates

--- remix.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@remix-run/dev').RemixConfig} */
-module.exports = {
-  browserNodeBuiltinsPolyfill: {
-    modules: {
-      buffer: true,
-      events: true,
-    },
-  },
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,28 @@
+import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vitePlugin as remix } from "@remix-run/dev";
+import react from "@vitejs/plugin-react";
+
+declare module "@remix-run/node" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
+    react(), // ✅ Ensures React support in Vite
+    tsconfigPaths(),
+  ],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
